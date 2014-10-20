65b304caa1ec91e7acc3c04f4e685504a4fb8408
keen <stamen0630@gmail.com>
2014-10-20 10:51:35 -0400

    templating

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var koa = require('koa');
 var router = require('koa-router');
 var app = koa();
 
+var fs = require('fs');
+var path = require('path');
+var marked = require('marked');
+var jade  = require('koa-jade');
+
 // logger
 app.use(function *(next) {
     var start = new Date;
@@ -10,10 +15,29 @@ app.use(function *(next) {
     console.log('%s - %s %s - %s ms', this.status,this.method, this.url, ms)
 });
 
+app.use(jade.middleware({
+    viewPath: __dirname + '/views',
+    pretty: true,
+    compileDebug: false,
+    basedir: 'views/layout.jade'
+}));
+
 app.use(router(app));
 
 app.get('/', function *() {
     this.body = 'Hello World';
+});
+
+app.get('/content/:file', function *() {
+    var fileName = this.params.file;
+    var doc = path.normalize('./' + ['content/',fileName,'.md'].join(''));
+    var exists = fs.existsSync(doc);
+    if(exists) {
+        var content = marked(fs.readFileSync(doc, "utf-8"));
+        yield this.render('content',{"title":fileName,"content":content},true);
+    } else {
+        this.throw(404,'Blog not found.');
+    }
 });
 
 app.listen(3000);
