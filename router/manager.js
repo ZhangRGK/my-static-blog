99b7008ac29e9d5911ee6b2c952d849ff5a59faf
张锐 <stamen0630@gmail.com>
2014-10-22 06:22:56 +0800

    debug hooks

--- router/manager.js
+++ router/manager.js
@@ -3,4 +3,6 @@ var exec = require('child_process').exec;
 exports.post = function* () {
     console.log("start pulling ...");
     exec("cd content && git pull -f master:master");
+    this.res.status=200;
+    this.res.body="pulling repo ...";
 }
