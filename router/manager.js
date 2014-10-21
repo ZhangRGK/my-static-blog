fd6f6ab4882d00870c329692ba9b754055c7f8ea
张锐 <stamen0630@gmail.com>
2014-10-22 06:35:19 +0800

    hooks fixed

--- router/manager.js
+++ router/manager.js
@@ -2,7 +2,6 @@ var exec = require('child_process').exec;
 
 exports.post = function* () {
     console.log("start pulling ...");
-    exec("cd content && git pull -f master:master");
-    this.res.status=200;
-    this.res.body="pulling repo ...";
+    exec("cd content && git pull -f origin master:master");
+    return this.body="pulling repo ..."
 }
