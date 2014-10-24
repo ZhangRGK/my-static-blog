235494a1bbe5b544e1e252af37d1aab47d1cbc9d
张锐 <stamen0630@gmail.com>
2014-10-24 17:00:06 +0800

    add nav

--- public/js/content.js
+++ public/js/content.js
@@ -1,4 +1,21 @@
-var codeprint = function() {
+$(function() {
     $('code, pre').addClass('prettyprint');
     prettyPrint();
-}();
+
+    var nav = $("nav");
+
+    $("#top").on("click", function() {
+        $("body").animate({scrollTop:0}, '500', 'swing');
+    });
+
+    $("h2").each(function(index,node) {
+        $(node).before("<a id='"+index+"'>");
+        var span = document.createElement('span');
+        $(span).html($(node).html())
+            .on("click", function() {
+                console.log($(node).offset());
+                $("body").animate({scrollTop:$(node).offset().top}, '700', 'swing');
+            });
+        nav.append(span);
+    });
+});
